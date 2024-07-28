e69c3b5a81944e66bd5a9ed31d094822d27f0e25
hywookim <hyeongwookim.dev@gmail.com>
2024-07-29 00:10:16 +0900

    style: 네비게이션 css 수정

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -8,19 +8,25 @@ export default function Navigation() {
   return (
     <nav className={styles.container}>
       <ul className={styles['nav-lists']}>
-        <li
-          className={
-            path === '/' ? styles['nav-list-active'] : styles['nav-list']
-          }
-        >
-          <Link href="/">HOME</Link>
+        <li>
+          <Link
+            className={`${styles['nav-list']} ${
+              path === '/' && styles['nav-list-active']
+            }`}
+            href="/"
+          >
+            HOME
+          </Link>
         </li>
-        <li
-          className={
-            path === '/about' ? styles['nav-list-active'] : styles['nav-list']
-          }
-        >
-          <Link href="/about">ABOUT</Link>
+        <li>
+          <Link
+            className={`${styles['nav-list']} ${
+              path === '/about' && styles['nav-list-active']
+            }`}
+            href="/about"
+          >
+            ABOUT
+          </Link>
         </li>
       </ul>
     </nav>
